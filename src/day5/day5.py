a6df8cf07ced376efdccd5d8e9797d6b02d33dc6
Chris Tran <christopher.v.tran@gmail.com>
2022-12-05 19:56:25 -0600

    feat: adds day5 part2 solution

--- src/day5/day5.py
+++ src/day5/day5.py
@@ -43,13 +43,28 @@ def parse_crane_directions(body: List[str]) -> List[List[int]]:
     ]
 
 
-def move_stacks(stacks: Dict[int, deque], crane_directions: List[List[int]]):
+def move_stacks(
+    stacks: Dict[int, deque], crane_directions: List[List[int]]
+) -> Dict[int, deque]:
     for directions in crane_directions:
         move_count, from_stack, to_stack = directions
 
         for _ in range(0, move_count):
             item = stacks[from_stack].pop()
             stacks[to_stack].append(item)
+
+    return stacks
+
+
+def move_stacks_part_2(
+    stacks: Dict[int, deque], crane_directions: List[List[int]]
+) -> Dict[int, deque]:
+    for directions in crane_directions:
+        move_count, from_stack, to_stack = directions
+
+        items_to_move = [stacks[from_stack].pop() for _ in range(0, move_count)]
+        for _ in range(0, len(items_to_move)):
+            stacks[to_stack].append(items_to_move.pop())
 
     return stacks
 
@@ -62,6 +77,9 @@ if __name__ == "__main__":
     header, body = parse_file()
     stacks = parse_stacks(header)
     crane_directions = parse_crane_directions(body)
-    final_stacks = move_stacks(stacks, crane_directions)
+
+    # final_stacks = move_stacks(stacks, crane_directions)
+    final_stacks = move_stacks_part_2(stacks, crane_directions)
+
     top_crates = get_top_crates(final_stacks)
     print(top_crates)
