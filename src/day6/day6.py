4b84544a03f54a461c16f12424d06c69c952c0ae
Chris Tran <christopher.v.tran@gmail.com>
2022-12-08 09:50:15 -0600

    feat: adds day6 part2 solution

--- src/day6/day6.py
+++ src/day6/day6.py
@@ -4,12 +4,12 @@ def parse_file() -> str:
     return line
 
 
-def get_start_of_packet_marker(message: str) -> int:
+def get_start_of_packet_marker(message: str, unique_character_count: int) -> int:
     start_of_packet_marker = 0
     for i in range(len(message)):
-        end_range = i + 4
+        end_range = i + unique_character_count
         set_of_chars = set(message[i:end_range])
-        if len(set_of_chars) == 4:
+        if len(set_of_chars) == unique_character_count:
             start_of_packet_marker = end_range
             break
 
@@ -18,5 +18,8 @@ def get_start_of_packet_marker(message: str) -> int:
 
 if __name__ == "__main__":
     message = parse_file()
-    start_of_packet_marker = get_start_of_packet_marker(message)
+    start_of_packet_marker = get_start_of_packet_marker(message, 4)
+    print(start_of_packet_marker)
+
+    start_of_packet_marker = get_start_of_packet_marker(message, 14)
     print(start_of_packet_marker)
